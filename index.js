7a27d351414d1b670460ad6b1d4968cf544021eb
Martyna <martyna.glaz@gmail.com>
2019-04-09 01:29:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string){
   console.log("hello")
 }
 function sayHiToGrandma(string){
-  return string.toLowerCase()
+  return ("hello").toLowerCase()
   console.log ("I can't hear you!")
 }
 function sayHiToGrandma(string){
