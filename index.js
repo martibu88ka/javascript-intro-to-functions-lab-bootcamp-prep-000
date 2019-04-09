f00335f33a47d3451b374fa20a0f944d1c077d29
Martyna <martyna.glaz@gmail.com>
2019-04-09 02:30:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function sayHiToGrandma(string) {
    }
 function sayHiToGrandma(string) {
      if (string === string.toUpperCase())
-        return(`YES INDEED!`)
+        return "YES INDEED!"
       }
 function sayHiToGrandma(string){
   if (string === `I love you, Grandma.`)
-return (`I love you, too.`)
+return "I love you, too."
 }
