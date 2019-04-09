cd638800c34ff4a1994dc0987bad132a37fce287
Martyna <martyna.glaz@gmail.com>
2019-04-09 01:28:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,3 @@ function sayHiToGrandma(string){
   return string.toUpperCase()
   console.log ("YES INDEED!")
 }
-function sayHiToGrandma(string){
-  console.log ("I love you, Grandma.")
-  console.log ("I love you, too.")
-}
