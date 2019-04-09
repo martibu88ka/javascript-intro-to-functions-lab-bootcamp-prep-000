c5dbcdf2d4e6916def0e3f934f959b103797269a
Martyna <martyna.glaz@gmail.com>
2019-04-09 02:02:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function logWhisper(string){
   console.log("hello")
 }
 function sayHiToGrandma(string){
-  if string===uppercase
-  return "YES INDEED!"
-  if string===lowercase
-  return "I can't hear you!"
+  if (string===(string.uppercase))
+  return ("YES INDEED!")
+  if (string===(string.lowercase))
+  return ("I can't hear you!")
   if string==="I love you, Grandma."
   return "I love you, too."
 }
