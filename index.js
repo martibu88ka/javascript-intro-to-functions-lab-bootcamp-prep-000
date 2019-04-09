b3b8bcddbbb743641206872f226e19ec7214a838
Martyna <martyna.glaz@gmail.com>
2019-04-09 01:39:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   console.log (string.toLowerCase())
   return "I can't hear you!"
-}
+
 function sayHiToGrandma(string){
   console.log (string.toUpperCase())
   return "YES INDEED!"
